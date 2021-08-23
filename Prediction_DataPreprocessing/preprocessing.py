53c4c548f03728d180dc6f37cc0386c53bd3f8b5
Sudharsan Baskar <71257512+sudharsanbaskars@users.noreply.github.com>
2021-08-23 09:52:12 +0530

    Update preprocessing.py

--- Prediction_DataPreprocessing/preprocessing.py
+++ Prediction_DataPreprocessing/preprocessing.py
@@ -5,40 +5,40 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 
 
-# from Application_Logging.logger import App_Logger
+from Application_Logging.logger import App_Logger
 
 
 class Preprocessor:
 
     def __init__(self):
-        # self.file_object = 'Prediction_Preprocessing.txt'
-        # self.logger_object = App_Logger()
+        self.file_object = 'Prediction_Preprocessing.txt'
+        self.logger_object = App_Logger()
         pass
 
     def order_columns_alphabetical_order(self, df):
         try:
             df.sort_index(axis="columns", inplace=True)
-            # self.logger_object.log(self.file_object, "Columns are ordered Succesfully")
+            self.logger_object.log(self.file_object, "Columns are ordered Succesfully")
             return df
 
         except Exception as e:
-            # self.logger_object.log(self.file_object, "Failed in ordering the columns in prediction file "+str(e))
+            self.logger_object.log(self.file_object, "Failed in ordering the columns in prediction file "+str(e))
             raise e
 
     def remove_unwanted_spaces(self,data):
 
-        # self.logger_object.log(self.file_object, 'Entered the remove_unwanted_spaces method of the Preprocessor class')
+        self.logger_object.log(self.file_object, 'Entered the remove_unwanted_spaces method of the Preprocessor class')
         self.data = data
         try:
             self.df_without_spaces = self.data.apply(
                 lambda x: x.str.strip() if x.dtype == "object" else x)  # drop the labels specified in the columns
-            # self.logger_object.log(self.file_object,'Unwanted spaces removal Successful.Exited the remove_unwanted_spaces method of the Preprocessor class')
+            self.logger_object.log(self.file_object,'Unwanted spaces removal Successful.Exited the remove_unwanted_spaces method of the Preprocessor class')
             return self.df_without_spaces
 
         except Exception as e:
-            # self.logger_object.log(self.file_object,'Exception occured in remove_unwanted_spaces method of the Preprocessor class. Exception message:  ' + str(e))
+            self.logger_object.log(self.file_object,'Exception occured in remove_unwanted_spaces method of the Preprocessor class. Exception message:  ' + str(e))
 
-            # self.logger_object.log(self.file_object,'unwanted space removal Unsuccessful. Exited the remove_unwanted_spaces method of the Preprocessor class')
+            self.logger_object.log(self.file_object,'unwanted space removal Unsuccessful. Exited the remove_unwanted_spaces method of the Preprocessor class')
 
             raise Exception()
 
@@ -50,7 +50,7 @@ class Preprocessor:
                     On Failure: Raise Exception
 
         """
-        # self.logger_object.log(self.file_object, 'Entered the impute_missing_values method of the Preprocessor class')
+        self.logger_object.log(self.file_object, 'Entered the impute_missing_values method of the Preprocessor class')
         self.data= data
         self.cols_with_missing_values=cols_with_missing_values
         try:
@@ -62,33 +62,33 @@ class Preprocessor:
                 else:
                     self.data[col] = self.data.fillna(self.data[col].median())
 
-            # self.logger_object.log(self.file_object, 'Imputing missing values Successful. Exited the impute_missing_values method of the Preprocessor class')
+            self.logger_object.log(self.file_object, 'Imputing missing values Successful. Exited the impute_missing_values method of the Preprocessor class')
             return self.data
 
         except Exception as e:
-            # self.logger_object.log(self.file_object,'Exception occured in impute_missing_values method of the Preprocessor class. Exception message:  ' + str(e))
-            # self.logger_object.log(self.file_object,'Imputing missing values failed. Exited the impute_missing_values method of the Preprocessor class')
+            self.logger_object.log(self.file_object,'Exception occured in impute_missing_values method of the Preprocessor class. Exception message:  ' + str(e))
+            self.logger_object.log(self.file_object,'Imputing missing values failed. Exited the impute_missing_values method of the Preprocessor class')
             raise Exception()
 
     def remove_unwanted_columns(self,data,columns):
 
-        # self.logger_object.log(self.file_object, 'Entered the remove_columns method of the Preprocessor class')
+        self.logger_object.log(self.file_object, 'Entered the remove_columns method of the Preprocessor class')
         self.data = data
         self.columns = columns
         try:
             self.useful_data = self.data.drop(labels=self.columns, axis=1)  # drop the labels specified in the columns
-            # self.logger_object.log(self.file_object, 'Column removal Successful.Exited the remove_columns method of the Preprocessor class')
+            self.logger_object.log(self.file_object, 'Column removal Successful.Exited the remove_columns method of the Preprocessor class')
             return self.useful_data
         except Exception as e:
-            # self.logger_object.log(self.file_object,'Exception occured in remove_columns method of the Preprocessor class. Exception message:  '+str(e))
-            # self.logger_object.log(self.file_object,'Column removal Unsuccessful. Exited the remove_columns method of the Preprocessor class')
+            self.logger_object.log(self.file_object,'Exception occured in remove_columns method of the Preprocessor class. Exception message:  '+str(e))
+            self.logger_object.log(self.file_object,'Column removal Unsuccessful. Exited the remove_columns method of the Preprocessor class')
 
             raise Exception()
 
 
     def is_null_present(self,data):
 
-        # self.logger_object.log(self.file_object, 'Entered the is_null_present method of the Preprocessor class')
+        self.logger_object.log(self.file_object, 'Entered the is_null_present method of the Preprocessor class')
         self.data=data
         self.null_present = False
         self.cols_with_missing_values = []
@@ -104,16 +104,16 @@ class Preprocessor:
                 self.dataframe_with_null['columns'] = self.data.columns
                 self.dataframe_with_null['missing values count'] = np.asarray(self.data.isna().sum())
                 self.dataframe_with_null.to_csv('preprocessing_data/null_values.csv') # storing the null column information to file
-            # self.logger_object.log(self.file_object,'Finding missing values is a success.Data written to the null values file. Exited the is_null_present method of the Preprocessor class')
+            self.logger_object.log(self.file_object,'Finding missing values is a success.Data written to the null values file. Exited the is_null_present method of the Preprocessor class')
             return self.null_present, self.cols_with_missing_values
         except Exception as e:
-            # self.logger_object.log(self.file_object,'Exception occured in is_null_present method of the Preprocessor class. Exception message:  ' + str(e))
-            # self.logger_object.log(self.file_object,'Finding missing values failed. Exited the is_null_present method of the Preprocessor class')
+            self.logger_object.log(self.file_object,'Exception occured in is_null_present method of the Preprocessor class. Exception message:  ' + str(e))
+            self.logger_object.log(self.file_object,'Finding missing values failed. Exited the is_null_present method of the Preprocessor class')
             raise Exception()
 
     def scale_numerical_columns(self,data):
 
-        # self.logger_object.log(self.file_object,'Entered the scale_numerical_columns method of the Preprocessor class')
+        self.logger_object.log(self.file_object,'Entered the scale_numerical_columns method of the Preprocessor class')
         self.data=data
 
         try:
@@ -122,17 +122,17 @@ class Preprocessor:
             self.scaled_data = self.scaler.fit_transform(self.num_df)#scaling the numerical values
             self.scaled_num_df = pd.DataFrame(data=self.scaled_data, columns=self.num_df.columns)
 
-            # self.logger_object.log(self.file_object, 'scaling for numerical values successful. Exited the scale_numerical_columns method of the Preprocessor class')
+            self.logger_object.log(self.file_object, 'scaling for numerical values successful. Exited the scale_numerical_columns method of the Preprocessor class')
             return self.scaled_num_df
 
         except Exception as e:
-            # self.logger_object.log(self.file_object,'Exception occured in scale_numerical_columns method of the Preprocessor class. Exception message:  ' + str(e))
-            # self.logger_object.log(self.file_object, 'scaling for numerical columns Failed. Exited the scale_numerical_columns method of the Preprocessor class')
+            self.logger_object.log(self.file_object,'Exception occured in scale_numerical_columns method of the Preprocessor class. Exception message:  ' + str(e))
+            self.logger_object.log(self.file_object, 'scaling for numerical columns Failed. Exited the scale_numerical_columns method of the Preprocessor class')
             raise Exception()
 
     def encode_categorical_columns(self,data):
 
-        # self.logger_object.log(self.file_object, 'Entered the encode_categorical_columns method of the Preprocessor class')
+        self.logger_object.log(self.file_object, 'Entered the encode_categorical_columns method of the Preprocessor class')
         self.data=data
 
         try:
@@ -141,12 +141,12 @@ class Preprocessor:
             for col in self.cat_df.columns:
                 self.cat_df = pd.get_dummies(self.cat_df, columns=[col], prefix=[col], drop_first=True)
 
-            # self.logger_object.log(self.file_object, 'encoding for categorical values successful. Exited the encode_categorical_columns method of the Preprocessor class')
+            self.logger_object.log(self.file_object, 'encoding for categorical values successful. Exited the encode_categorical_columns method of the Preprocessor class')
             return self.cat_df
 
         except Exception as e:
-            # self.logger_object.log(self.file_object,'Exception occured in encode_categorical_columns method of the Preprocessor class. Exception message:  ' + str(e))
-            # self.logger_object.log(self.file_object, 'encoding for categorical columns Failed. Exited the encode_categorical_columns method of the Preprocessor class')
+            self.logger_object.log(self.file_object,'Exception occured in encode_categorical_columns method of the Preprocessor class. Exception message:  ' + str(e))
+            self.logger_object.log(self.file_object, 'encoding for categorical columns Failed. Exited the encode_categorical_columns method of the Preprocessor class')
             raise Exception()
 
 

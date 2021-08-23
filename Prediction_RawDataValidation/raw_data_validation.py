ae2fa4cf005aa3da7b1df015ba0cea056b5b5ab4
Sudharsan Baskar <71257512+sudharsanbaskars@users.noreply.github.com>
2021-08-23 09:53:19 +0530

    Update raw_data_validation.py

--- Prediction_RawDataValidation/raw_data_validation.py
+++ Prediction_RawDataValidation/raw_data_validation.py
@@ -4,7 +4,7 @@ import os
 import shutil
 import json
 import pandas as pd
-# from Application_Logging.logger import App_Logger
+from Application_Logging.logger import App_Logger
 
 
 class RawDataValidation:
@@ -18,7 +18,7 @@ class RawDataValidation:
 
     def __init__(self, path):
         self.logging_file_name = 'Prediction_RawDataValidation.txt'
-        # self.log_writer = App_Logger()
+        self.log_writer = App_Logger()
         self.batch_path = path
         self.schema_path = 'schema_prediction.json'
 
@@ -32,15 +32,15 @@ class RawDataValidation:
             no_of_columns = dict["NumberofColumns"]
             column_names = dict["ColName"]
 
-            # self.log_writer.log(self.logging_file_name,
-            #                     "Got the values from scehema JSON file."
-            #                                             "The number of columns are "+str(no_of_columns))
+            self.log_writer.log(self.logging_file_name,
+                                "Got the values from scehema JSON file."
+                                                        "The number of columns are "+str(no_of_columns))
             return no_of_columns, column_names
 
         except Exception as e:
-            # self.log_writer.log(self.logging_file_name,
-            #                     "Error occured in getting values from schema."
-            #                                             "The exception is "+str(e))
+            self.log_writer.log(self.logging_file_name,
+                                "Error occured in getting values from schema."
+                                                        "The exception is "+str(e))
             raise e
 
 
@@ -55,14 +55,14 @@ class RawDataValidation:
             if os.path.isdir(bad_folder_path) == False:
                 os.makedirs(bad_folder_path)
 
-            # self.log_writer.log(self.logging_file_name,
-            #                     "Successfully created Directory for good and bad data folder")
+            self.log_writer.log(self.logging_file_name,
+                                "Successfully created Directory for good and bad data folder")
 
 
 
         except Exception as e:
-            # self.log_writer.log(self.logging_file_name,
-            #                     "Something went wrong in creating good and bad data folders.The exception is "+str(e))
+            self.log_writer.log(self.logging_file_name,
+                                "Something went wrong in creating good and bad data folders.The exception is "+str(e))
             raise e
 
 
@@ -71,13 +71,13 @@ class RawDataValidation:
             path = 'Prediction_ValidatedRawData/'
             if os.path.isdir(path+'GoodDataFolder/'):
                 shutil.rmtree(path+'GoodDataFolder/')
-            # self.log_writer.log(self.logging_file_name,
-            #                     "Good Data Folder Deleted Successfilly!")
+            self.log_writer.log(self.logging_file_name,
+                                "Good Data Folder Deleted Successfilly!")
 
 
         except OSError as e:
-            # self.log_writer.log(self.logging_file_name,
-            #                     "Something went wrong in deleting existing good data folder.The exception is "+str(e))
+            self.log_writer.log(self.logging_file_name,
+                                "Something went wrong in deleting existing good data folder.The exception is "+str(e))
             raise e
 
 
@@ -86,12 +86,12 @@ class RawDataValidation:
             path = 'Prediction_ValidatedRawData/'
             if os.path.isdir(path + 'BadDataFolder/'):
                 shutil.rmtree(path + 'BadDataFolder/')
-            # self.log_writer.log(self.logging_file_name,
-            #                     "Bad Data Folder Deleted Successfilly!")
+            self.log_writer.log(self.logging_file_name,
+                                "Bad Data Folder Deleted Successfilly!")
 
         except OSError as e:
-                # self.log_writer.log(self.logging_file_name,
-                #                     "Something went wrong in deleting existing Bad data folder.The exception is "+str(e))
+                self.log_writer.log(self.logging_file_name,
+                                    "Something went wrong in deleting existing Bad data folder.The exception is "+str(e))
                 raise e
 
 
@@ -112,18 +112,18 @@ class RawDataValidation:
                 for file in files:
                     if file not in os.listdir(dest):
                         shutil.move(source+file, dest)
-                # self.log_writer.log(self.logging_file_name,
-                #                     "Bad raw files successfully move to archive folder")
+                self.log_writer.log(self.logging_file_name,
+                                    "Bad raw files successfully move to archive folder")
 
 
         except OSError as e:
-            # self.log_writer.log(self.logging_file_name,
-            #                     "Something went wrong in deleting existing Bad data folder.The exception is " + str(e))
+            self.log_writer.log(self.logging_file_name,
+                                "Something went wrong in deleting existing Bad data folder.The exception is " + str(e))
             raise e
 
         except Exception as e:
-            # self.log_writer.log(self.logging_file_name,
-            #                     "Something went wrong in deleting existing Bad data folder.The exception is " + str(e))
+            self.log_writer.log(self.logging_file_name,
+                                "Something went wrong in deleting existing Bad data folder.The exception is " + str(e))
             raise e
 
     def validate_column_length(self, NumberOfColumns):
@@ -143,27 +143,27 @@ class RawDataValidation:
                 no_of_columns = df.shape[1]
                 if no_of_columns == NumberOfColumns:
                     shutil.copy(file_path, GoodDataFolderPath)
-                    # self.log_writer.log(self.logging_file_name,
-                    #                     "Valid file Name!! File successfully moved to Good Data Folder")
+                    self.log_writer.log(self.logging_file_name,
+                                        "Valid file Name!! File successfully moved to Good Data Folder")
                     return True
                 else:
                     shutil.copy(file_path, BadDataFolderPath)
-                    # self.log_writer.log(self.logging_file_name,
-                    #                     "Invalid File!! File successfully moved to Bad Data Folder")
+                    self.log_writer.log(self.logging_file_name,
+                                        "Invalid File!! File successfully moved to Bad Data Folder")
 
-                    # self.log_writer.log(self.logging_file_name,
-                    #                 "File column length validation completed!!")
+                    self.log_writer.log(self.logging_file_name,
+                                    "File column length validation completed!!")
                     return False
 
 
         except OSError as e:
-            # self.log_writer.log(self.logging_file_name,
-            #                     "Failed in validating column length "+ str(e))
+            self.log_writer.log(self.logging_file_name,
+                                "Failed in validating column length "+ str(e))
             raise e
 
         except Exception as e:
-            # self.log_writer.log(self.logging_file_name,
-            #                     "Failed in validating column length " + str(e))
+            self.log_writer.log(self.logging_file_name,
+                                "Failed in validating column length " + str(e))
             raise e
 
 
@@ -181,14 +181,14 @@ class RawDataValidation:
                                     "Prediction_ValidatedRawData/BadDataFolder")
                         break
 
-            # self.log_writer.log(self.logging_file_name,
-            #                                 "Validation missing values in whole column completed!!")
+            self.log_writer.log(self.logging_file_name,
+                                            "Validation missing values in whole column completed!!")
 
 
 
         except Exception as e:
-            # self.log_writer.log(self.logging_file_name,
-            #                     "Failed in validating missing values in csv files " + str(e))
+            self.log_writer.log(self.logging_file_name,
+                                "Failed in validating missing values in csv files " + str(e))
             raise e
 
     def moveGoodFilesToFinalPrediction(self):
@@ -197,10 +197,10 @@ class RawDataValidation:
                 if os.path.isfile('Prediction_InputFileAfterValidation/'+file):
                     os.remove('Prediction_InputFileAfterValidation/'+file)
                 shutil.move('Prediction_ValidatedRawData/GoodDataFolder/'+file, 'Prediction_InputFileAfterValidation/')
-                # self.log_writer.log(self.logging_file_name, "Succesfully moved the good data file to Final input data folder!!")
+                self.log_writer.log(self.logging_file_name, "Succesfully moved the good data file to Final input data folder!!")
 
         except Exception as e:
-            # self.log_writer.log(self.logging_file_name, "Failed in moveGoodFilesToFinalPrediction "+str(e))
+            self.log_writer.log(self.logging_file_name, "Failed in moveGoodFilesToFinalPrediction "+str(e))
             raise e
 
 
@@ -208,3 +208,8 @@ class RawDataValidation:
 
         if os.path.exists('Prediction_FinalResultFile/final_result.csv'):
             os.remove('Prediction_FinalResultFile/final_result.csv')
+
+
+
+
+

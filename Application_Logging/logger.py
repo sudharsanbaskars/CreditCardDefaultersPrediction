9fcdd836bd2c0eb3222e6b97d39e772103eb0a34
Sudharsan Baskar <71257512+sudharsanbaskars@users.noreply.github.com>
2021-08-23 09:51:40 +0530

    Update logger.py

--- Application_Logging/logger.py
+++ Application_Logging/logger.py
@@ -10,10 +10,10 @@ class App_Logger:
         self.now = datetime.now()
         self.date = self.now.date()
         self.current_time = self.now.strftime("%H:%M:%S")
-        path = "F:/Data Science & AI/Internship Projects/CreditCardDefaulterPrediction/Training_Logs"
+        path = "Training_Logs"
         with open(path+"/"+str(file_name), 'a+') as f:
             f.write(str(self.date)+ "\t\t" + str(self.current_time) + "\t\t"+str(log_message)+"\n")
             f.close()
-        print(str(self.date)+ "\t\t" + str(self.current_time) + "\t\t" + str(log_message)+"\n")
+#         print(str(self.date)+ "\t\t" + str(self.current_time) + "\t\t" + str(log_message)+"\n")
 
 
